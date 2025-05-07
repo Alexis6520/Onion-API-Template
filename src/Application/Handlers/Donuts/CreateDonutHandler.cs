cdb7d4730b8cedb088af80a964ed1a3d62587d8a
Alexis6520 <alexis_nova@outlook.com>
2025-05-07 09:56:19 -0500

    Añadir registro con NLog en la creación de donuts
    Se ha implementado `ILogger<CreateDonutHandler>` en `CreateDonutHandler.cs` para registrar información sobre la creación de donuts, incluyendo el ID del donut creado. Se han agregado referencias a los paquetes `NLog` y `NLog.Web.AspNetCore` en `Host.csproj`. En `Program.cs`, se ha configurado NLog como proveedor de registro con un bloque `try-catch-finally` para manejar excepciones y asegurar el cierre adecuado del registro. Además, se ha creado un archivo de configuración `nlog.config` para definir la estructura y reglas de los mensajes de registro.

--- src/Application/Handlers/Donuts/CreateDonutHandler.cs
+++ src/Application/Handlers/Donuts/CreateDonutHandler.cs
@@ -4,13 +4,15 @@ using Domain.Entities;
 using Domain.Services;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Net;
 
 namespace Application.Handlers.Donuts
 {
-    public class CreateDonutHandler(AppDbContext dbContext) : IRequestHandler<CreateDonutCommand, Result<int>>
+    public class CreateDonutHandler(AppDbContext dbContext, ILogger<CreateDonutHandler> logger) : IRequestHandler<CreateDonutCommand, Result<int>>
     {
         private readonly AppDbContext _dbContext = dbContext;
+        private readonly ILogger<CreateDonutHandler> _logger = logger;
 
         public async Task<Result<int>> Handle(CreateDonutCommand request, CancellationToken cancellationToken)
         {
@@ -31,6 +33,7 @@ namespace Application.Handlers.Donuts
 
             _dbContext.Donuts.Add(donut);
             await _dbContext.SaveChangesAsync(default);
+            _logger.LogInformation("Dona creada con ID: {Id}", donut.Id);
             return Result<int>.Success(donut.Id, HttpStatusCode.Created);
         }
     }
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,30 +1,49 @@
 using Application;
 using Infrastructure;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.GetCurrentClassLogger();
 
-builder.Services
-    .AddAppServices()
-    .AddInfrastructure();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services
+        .AddAppServices()
+        .AddInfrastructure();
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "Programa detenido por excepción");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
